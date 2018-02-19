35517bfdc31d7f0a84d70ad767c9dd871af12816
parksw3 <parksw3@mcmaster.ca>
2018-02-19 18:57:01 -0500

    I think I made all the figures?

--- colors.R
+++ colors.R
@@ -1,3 +1,4 @@
-mlecolor <- "red"
-momcolor <- "green"
-limcolor <- "blue"
+mlecolor <- "#D95F02"
+momcolor <- "#1B9E77"
+limcolor <- "#7570B3"
+norcolor <- "#E7298A"
--- ebola_fig.R
+++ ebola_fig.R
@@ -15,6 +15,7 @@ infmean <- 5
 infshape <- 0.8
 numSamps <- 10000
 
+source("colors.R")
 source("euler.R")
 source("quantile.R")
 source("mle.R")
@@ -32,10 +33,31 @@ rho_eff <- Ghat*log(2)/doub
 ## draw r-R curve
 pdf("ebola.pdf", width=6, heigh=4) 
 GenCurve(gen, xmax, ymax, rho_eff, Reff, lwd=2)
+legend(
+	"topleft"
+	, legend=c("empirical", "moment")
+	, lty=c(1, 2)
+	, lwd=2
+	, seg.len=4
+	, col=c("black", momcolor)
+)
+dev.off()
+
+## normal approx
+pdf("ebola_normal.pdf", width=6, heigh=4) 
+NormalCurve(gen, xmax, ymax, rho_eff, Reff, lwd=2)
+legend(
+	"topleft"
+	, legend=c("empirical", "moment", "normal")
+	, lty=c(1, 1, 2)
+	, lwd=2
+	, seg.len=4
+	, col=c("black", momcolor, norcolor)
+)
 dev.off()
 
 nsamp <- 50
-nrep <- 10
+nrep <- 500
 
 samp_list <- replicate(nrep, {
 	s <- sample(gen, nsamp)
@@ -60,14 +82,26 @@ samp_df <- samp_list %>%
 		lwr=quantile(value, 0.025),
 		upr=quantile(value, 0.975),
 		mean=mean(value)
+	) %>%
+	mutate(key=factor(key, levels=c("moment", "mle")))
+
+ggebola <- (
+	ggplot(samp_df, aes(x=rho))
+	+ geom_ribbon(aes(ymin=lwr, ymax=upr, group=key, col=key, fill=key), alpha=0.2, lwd=1, color=FALSE)
+	+ geom_line(aes(y=mean, group=key, col=key), lwd=1, lty=2)
+	+ scale_color_manual(values=c(momcolor, mlecolor))
+	+ scale_fill_manual(values=c(momcolor, mlecolor))
+	+ stat_function(fun=function(x) EulerCurve(mean(gen)/x, gen), col="black", lty=1, lwd=1)
+	+ stat_function(fun=exp, col=limcolor, lty=3, lwd=1)
+	+ stat_function(fun=function(x) x+1, col=limcolor, lty=3, lwd=1)
+	+ geom_point(data=data.frame(rho=rho_eff, R=Reff), aes(y=R), pch=c(19, 17, 15), size=2)
+	+ scale_x_continuous(expression(Relative~length~of~generation~interval~(rho)), expand=c(0,0))
+	+ scale_y_continuous("Reproductive number") 
+	+ theme(
+		legend.title = element_blank(),
+		legend.position = c(0.1, 0.85),
+		panel.grid = element_blank()
 	)
+)
 
-ggplot(samp_df, aes(x=rho, group=key, col=key, fill=key)) +
-	geom_ribbon(aes(ymin=lwr, ymax=upr), alpha=0.2, col=NA) +
-	geom_line(aes(y=mean, group=key)) +
-	stat_function(fun=function(x) EulerCurve(mean(gen)/x, gen), col="black")
-
-	
-
-	
-	
+ggsave("ebola_sample.pdf", ggebola, width=6, height=4)
